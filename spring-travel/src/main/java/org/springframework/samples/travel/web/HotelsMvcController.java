393ff93da9d59fd9615a0a1c8b921ed488ab2970
Peter Ledbrook <peter@cacoethes.co.uk>
2012-10-14 14:35:03 -0400

    Remove tabs from files.

--- spring-travel/src/main/java/org/springframework/samples/travel/web/HotelsMvcController.java
+++ spring-travel/src/main/java/org/springframework/samples/travel/web/HotelsMvcController.java
@@ -18,33 +18,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HotelsMvcController {
 
- 	@Inject private BookingService bookingService;
+    @Inject private BookingService bookingService;
 
-	@RequestMapping(value = "/hotels/search", method = RequestMethod.GET)
-	public void search(SearchCriteria searchCriteria, Principal currentUser, Model model) {
-		if (currentUser != null) {
-			List<Booking> booking = bookingService.findBookings(currentUser.getName());
-			model.addAttribute(booking);
-		}
-	}
+    @RequestMapping(value = "/hotels/search", method = RequestMethod.GET)
+    public void search(SearchCriteria searchCriteria, Principal currentUser, Model model) {
+        if (currentUser != null) {
+            List<Booking> booking = bookingService.findBookings(currentUser.getName());
+            model.addAttribute(booking);
+        }
+    }
 
-	@RequestMapping(value = "/hotels", method = RequestMethod.GET)
-	public String list(SearchCriteria criteria, Model model) {
-		List<Hotel> hotels = bookingService.findHotels(criteria);
-		model.addAttribute(hotels);
-		return "hotels/list";
-	}
+    @RequestMapping(value = "/hotels", method = RequestMethod.GET)
+    public String list(SearchCriteria criteria, Model model) {
+        List<Hotel> hotels = bookingService.findHotels(criteria);
+        model.addAttribute(hotels);
+        return "hotels/list";
+    }
 
-	@RequestMapping(value = "/hotels/{id}", method = RequestMethod.GET)
-	public String show(@PathVariable Long id, Model model) {
-		model.addAttribute(bookingService.findHotelById(id));
-		return "hotels/show";
-	}
+    @RequestMapping(value = "/hotels/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute(bookingService.findHotelById(id));
+        return "hotels/show";
+    }
 
-	@RequestMapping(value = "/bookings/{id}", method = RequestMethod.DELETE)
-	public String deleteBooking(@PathVariable Long id) {
-		bookingService.cancelBooking(id);
-		return "redirect:../hotels/search";
-	}
+    @RequestMapping(value = "/bookings/{id}", method = RequestMethod.DELETE)
+    public String deleteBooking(@PathVariable Long id) {
+        bookingService.cancelBooking(id);
+        return "redirect:../hotels/search";
+    }
 
 }
