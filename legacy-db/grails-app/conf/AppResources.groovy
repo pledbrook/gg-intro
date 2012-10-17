21bcb21793dcc3ac9ce6280a1c64f09e3aa89641
Peter Ledbrook <peter@cacoethes.co.uk>
2012-10-17 08:46:43 -0400

    Update main layout to use Resources.

---
+++ legacy-db/grails-app/conf/AppResources.groovy
@@ -0,0 +1,9 @@
+modules = {
+    master {
+        dependsOn "jquery"
+        resource url: "js/application.js"
+        resource url: "css/main.css"
+        resource url: "css/mobile.css"
+    }
+    
+}
